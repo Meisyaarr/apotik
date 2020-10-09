3dae4ee4cc9c3a54d64e6d91c3ae90d437ce4bce
Meisyaarr <meisyayuniar81@gmail.com>
2020-10-09 08:47:43 -0700

    first commit

---
+++ database/migrations/2020_09_09_000538_create_barang_masuk_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBarangMasukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang_masuk', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang_masuk');
+    }
+}
